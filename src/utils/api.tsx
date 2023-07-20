9d2c4aab8ae9ac8caac59cc5b0b1d63a102322c7
nemajeri <nemanja.jerinic832@gmail.com>
2023-07-20 12:07:43 +0200

    Added toast for better usability and error handling

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -3,17 +3,21 @@ import {
   BINANCE_API_URL,
   FETCH_FAILURE_MESSAGE,
   TEST_ENVIRONMENT,
+  FETCH_SUCCESS_MESSAGE,
 } from './constants';
 import { Ticker } from './interfaces';
+import { toast } from 'react-toastify';
 
 const fetchData = async (): Promise<Ticker[] | undefined> => {
   try {
     const response = await axios.get<Ticker[]>(BINANCE_API_URL);
+    toast.success(FETCH_SUCCESS_MESSAGE);
     return response.data;
   } catch (error) {
     if (process.env.NODE_ENV === TEST_ENVIRONMENT) {
       console.error(`${FETCH_FAILURE_MESSAGE}:`, error);
     }
+    toast.error(FETCH_FAILURE_MESSAGE);
     throw error;
   }
 };
--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,4 +1,5 @@
 export const BINANCE_API_URL: string =
   'https://data.binance.com/api/v3/ticker/24hr';
 export const TEST_ENVIRONMENT: string = 'test';
-export const FETCH_FAILURE_MESSAGE: string = 'Unable to fetch items.';
+export const FETCH_FAILURE_MESSAGE: string = 'Unable to fetch tickers.';
+export const FETCH_SUCCESS_MESSAGE: string = 'Tickers fetched succesfully.';
